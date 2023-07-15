df3a6a33ab19803b332984338ed011658685aa93
MeqdadDev <meqdad.darweesh@gmail.com>
2023-07-15 22:45:02 +0300

    basic data types in java

---
+++ 1. data_types.java
@@ -0,0 +1,33 @@
+package com.mycompany.firstproject;
+
+/**
+ * sout+tab -> System.out.print
+ */
+public class Firstproject {
+
+    public static void main(String[] args) {
+        int age = 25;
+        byte x = 127; // 8-bit: -128 -> 127
+        byte bintodec = 0b1111111;
+        float currency = 3.73f;
+        double area = 3432.12343456d; // or use f at the end
+        long population = 1234564323;
+        char gender = 'M';
+        boolean readiness = true;
+        String name = "Meqdad 'Ahmad' Darwish";
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(x);
+        System.out.println(bintodec);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(currency);
+        System.out.println(population);
+        System.out.println(area);
+        System.out.println(gender);
+        System.out.println(readiness);
+
+        // Casting
+        System.out.println((float) x / 2);
+
+    }
+}
