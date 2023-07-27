7055c9c914644ce8c27224d49355f7aed34d3c02
MeqdadDev <meqdad.darweesh@gmail.com>
2023-07-27 09:10:53 +0300

    move basic snippets into basic folder

---
+++ 1. basics/7. varargs-expenses.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Firstproject {
+
+    public static float CalcExpenses(float... e) {
+        float total = 0f;
+        for (float i : e) {
+            total += i;
+        }
+
+        return total;
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to Expenses Claculator");
+        System.out.println("Enter your expenses for this month: ");
+
+        System.out.print("Rent = ");
+        float e1 = input.nextFloat();
+
+        System.out.print("Electricity = ");
+        float e2 = input.nextFloat();
+
+        float result1 = CalcExpenses(e1, e2);
+
+        System.out.println("Total for now = " + result1 + "$");
+
+        System.out.print("Internet = ");
+        float e3 = input.nextFloat();
+
+        float result2 = CalcExpenses(e1, e2, e3);
+
+        System.out.println("Total for now = " + result2 + "$");
+
+    }
+}
